9d7fd0e9935ec687448071dbe1ce620338410cf4
kingveee <vivianpham22@gmail.com>
2017-08-11 22:23:39 -0400

    fixed

--- LuckyNumberProject/Program.cs
+++ LuckyNumberProject/Program.cs
@@ -12,28 +12,43 @@ namespace LuckyNumberProject
         {
             int jackpot = 100000;
             int lessJackpot = 200;
+
             // PART 1
-            Console.WriteLine("Welcome to Lucky's Number!");
-            Console.WriteLine("Today's Jackpot is: $" + jackpot);
-            Console.WriteLine("Please enter a low number in the number range.");
+            Console.ForegroundColor = ConsoleColor.Red;
+            string text = "Welcome To Lucky's Jackpot!";
+            Console.Write(new string(' ', (Console.WindowWidth - text.Length) / 2));
+            Console.WriteLine(text);
+            Console.ResetColor();
+
+            string jackpot2 = "Today's Jackpot is: $ ";
+            Console.Write(new string(' ', (Console.WindowWidth - text.Length) / 2));
+            Console.WriteLine(jackpot2 + jackpot);
+
+
+            Console.WriteLine("\nPlease enter a low number that is 0 or higher");
             int lowNumber = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter a high whole number in the number range.");
+            Console.WriteLine("\nPlease enter a higher number");
             int highNumber = int.Parse(Console.ReadLine());
 
 
             // This is the loop 
             // Asking for 6 numbers
             int[] sixGuess = new int[6];
+            Console.WriteLine("\nPick your 6 LUCKY numbers!");
             for (int i = 0; i <= sixGuess.Length - 1; i++)
             {
-                {
-                    Console.WriteLine("Enter one number");          
+                   
+                {                             
+                    Console.WriteLine("Enter a number");
                     sixGuess[i] = int.Parse(Console.ReadLine());
+          
                 }
 
                 while (sixGuess[i] < lowNumber || sixGuess[i] > highNumber)
                 {
-                    Console.WriteLine("Please enter a number between:" + lowNumber + "-" + highNumber);
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nPlease enter a number between: " + lowNumber + "-" + highNumber);
+                    Console.ResetColor();
                     break;
                 }
 
@@ -46,15 +61,15 @@ namespace LuckyNumberProject
             int[] luckyNumber = new int[6];
             for (int j = 0; j < luckyNumber.Length; j++)
             {
-                luckyNumber[j] = randomNumber.Next(lowNumber, highNumber); //<-- was i suppose to put a +1 or ++ at the end of highNumber?
-                
+                luckyNumber[j] = randomNumber.Next(lowNumber, highNumber);
+
             }
             foreach (int num in luckyNumber) //<---printing element in luckyNumber array
             {
-                Console.WriteLine("Lucky Number:" + num); //+ luckyNumber[j]); // + "Your Numbers:" + sixGuess[j]); 
-                // why couldnt i have just put  + luckyNumbers ^
+                Console.WriteLine("Lucky Number:" + num); 
+
             }
-           
+
 
             // PART 3
             // HARD CODE || Numbers correctly guessed
@@ -77,40 +92,40 @@ namespace LuckyNumberProject
                     }
                 }
             }
-            Console.WriteLine("You guessed " + correctNumbers + " numbers correctly");
+            Console.WriteLine("\nYou guessed " + correctNumbers + " numbers correctly");
 
             if (correctNumbers == 6)
             {
-                Console.WriteLine("YOU DID IT! YOU WON " + jackpot + "!");
+                Console.WriteLine("\nYOU DID IT! YOU WON " + jackpot + "!");
             }
             else if (correctNumbers >= 5)
             {
-                Console.WriteLine("Great job you! You just won $" + lessJackpot);
+                Console.WriteLine("\nGreat job you! You just won $" + lessJackpot);
             }
-            else if (correctNumbers >= 4) 
+            else if (correctNumbers >= 4)
             {
-                Console.WriteLine("Sweet! You just won $" + lessJackpot / 2);
+                Console.WriteLine("\nSweet! You just won $" + lessJackpot / 2);
             }
             else if (correctNumbers >= 0)
             {
-                Console.WriteLine("Sorry! You didn't hit the jackpot. \nThere's always next time!");
+                Console.WriteLine("\nSorry! You didn't hit the jackpot. \nThere's always next time!");
             }
-         
+
 
             // Ask the player if they want to play again
             string playAgain;
             do
             {
-                Console.WriteLine("Would you like to play again? YES/NO");
+                Console.WriteLine("\nWould you like to play again? YES/NO");
                 playAgain = Console.ReadLine().ToUpper();
                 if (playAgain == "NO")
                 {
-                    Console.WriteLine("Thanks for playing!");
+                    Console.WriteLine("\nThanks for playing!");
                 }
 
             }
             while (playAgain == "YES");
-
+            return;
 
 
 
